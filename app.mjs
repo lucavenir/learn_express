20056e6c9058f46c86d6650a0c831501a743125c
venir.dev <luca@venir.dev>
2023-11-03 00:03:33 +0100

    random middleware appears

--- app.mjs
+++ app.mjs
@@ -13,6 +13,14 @@ async function start() {
         await connection(process.env.MONGO_URI);
         console.log("db connected");
         console.log("starting our server..");
+        app.use((req, res, next) => {
+            console.log('Time: ', Date.now());
+            next();
+        });
+        app.use('/', (req, res, next) => {
+            console.log('request url: ', req.originalUrl);
+            next();
+        })
         app.listen(port, () => {
             console.log(`server running on port ${port}`);
         });
