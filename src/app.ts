5dfadae66036ceb6c30394540c6db9df78de8ebf
venir.dev <luca@venir.dev>
2023-11-06 21:46:40 +0100

    removed useless or

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ async function start() {
    // startup
    dotenv.config();
    const app = express();
-   const port = process.env.PORT || 3000;
+   const port = process.env.PORT;
 
    console.log("db connection..");
    await connection(process.env.MONGO_URI!);
