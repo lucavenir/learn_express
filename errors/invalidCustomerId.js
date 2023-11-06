1ab0837cda548fe040ac45b5416f25aebcee4c5b
venir.dev <luca@venir.dev>
2023-11-06 23:03:02 +0100

    cleanup

--- errors/invalidCustomerId.js
+++
@@ -1,9 +0,0 @@
-class InvalidCustomerId extends Error {
-  constructor(customerId) {
-    const message = `Invalid customer id: ${customerId}`;
-    super(message);
-    this.statusCode = 400;
-  }
-}
-
-export default InvalidCustomerId;
