d38793c4324dc272598708a2cc4b3f4aa39458e8
venir.dev <luca@venir.dev>
2023-11-07 00:03:22 +0100

    and let there be registration

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,31 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import dotenv from "dotenv";
 import connection from "./db/connect";
+import { serve as swaggerServe, setup as swaggerSetup } from "swagger-ui-express";
+import * as swaggerJson from "./tsoa/tsoa.json"
+import { RegisterRoutes } from "./routes/routes";
 
 async function start() {
    // startup
    dotenv.config();
-   const app = express();
    const port = process.env.PORT;
 
-   console.log("db connection..");
+   const app = express();
+   app.use(urlencoded({ extended: true }));
+   app.use(json());
+   RegisterRoutes(app);
+   app.use(
+      ["/openapi", "/docs", "/swagger"],
+      swaggerServe,
+      swaggerSetup(swaggerJson)
+   );
+
+   app.get("/swagger.json", (_, res) => {
+      res.setHeader("Content-Type", "application/json");
+      res.sendFile(`${__dirname}/tsoa/tsoa.json`);
+   });
+
    await connection(process.env.MONGO_URI!);
-   console.log("db connected");
-   console.log("starting our server..");
 
    app.listen(port, () => {
       console.log(`server running on port ${port}`);
